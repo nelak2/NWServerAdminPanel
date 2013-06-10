538c538c3bcb4dcfede49eb55b519b67eb08c18e
orizvi <hello468@hotmail.com>
2013-06-10 09:33:11 -0600

    Initial Commit

---
+++ Controllers/MainController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NWServerAdminPanel.Controllers
+{
+    public class MainController : Controller
+    {
+        //
+        // GET: /Main/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Main/Welcome/
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+
+    }
+}
